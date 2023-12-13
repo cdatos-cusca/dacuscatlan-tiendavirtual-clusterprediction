0b02ecdbe7f02a2874c688dc55a0dd128148b038
cdatos-cusca <153763087+cdatos-cusca@users.noreply.github.com>
2023-12-13 18:31:14 +0000

    Cosas

--- pages/ExtraYA.py
+++ pages/ExtraYA.py
@@ -23,3 +23,32 @@ if uploaded_file_extra is not None:
         file_name='data_clusters_extraya.csv',
         mime='text/csv',
     )
+
+    footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Hecho con  ❤ por Data & Analytics</a></p>
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
